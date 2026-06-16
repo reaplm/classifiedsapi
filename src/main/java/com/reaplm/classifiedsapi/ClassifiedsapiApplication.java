2c4a1a3de2ec1c7beb3750709146613b6d5fe226
Pearl Molefe <pdm.molefe@gmail.com>
2026-06-16 10:28:48 +0100

    initial commit

---
+++ src/main/java/com/reaplm/classifiedsapi/ClassifiedsapiApplication.java
@@ -0,0 +1,13 @@
+package com.reaplm.classifiedsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassifiedsapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClassifiedsapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/reaplm/classifiedsapi/ClassifiedsapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reaplm.classifiedsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassifiedsapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
