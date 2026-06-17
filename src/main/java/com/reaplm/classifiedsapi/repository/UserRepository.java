6a58a6a4ae88cbc06d309ba7a875f50088d4fdd9
Pearl Molefe <pdm.molefe@gmail.com>
2026-06-17 13:00:26 +0100

    change Long to UUID

--- src/main/java/com/reaplm/classifiedsapi/repository/UserRepository.java
+++ src/main/java/com/reaplm/classifiedsapi/repository/UserRepository.java
@@ -1,11 +1,12 @@
 package com.reaplm.classifiedsapi.repository;
 
 import java.util.Optional;
+import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.reaplm.classifiedsapi.model.User;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, UUID> {
     Optional<User> findByUsername(String username);
 }
