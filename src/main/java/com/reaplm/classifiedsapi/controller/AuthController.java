31ec903a4a4796b84f3070c76d3ca8ad0d665de7
Pearl Molefe <pdm.molefe@gmail.com>
2026-06-21 21:02:33 +0100

    use record type for AuthResponse

--- src/main/java/com/reaplm/classifiedsapi/controller/AuthController.java
+++ src/main/java/com/reaplm/classifiedsapi/controller/AuthController.java
@@ -4,8 +4,10 @@ import java.time.OffsetDateTime;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.reaplm.classifiedsapi.dto.AuthResponse;
 import com.reaplm.classifiedsapi.dto.LoginRequest;
 import com.reaplm.classifiedsapi.dto.RegistrationRequest;
 import com.reaplm.classifiedsapi.model.User;
@@ -26,6 +29,7 @@ import jakarta.persistence.Column;
 public class AuthController {
 
     @Autowired private AuthenticationManager authenticationManager;
+    @Autowired private AuthenticationManager AuthResponse;
     @Autowired private UserRepository userRepository;
     @Autowired private PasswordEncoder passwordEncoder;
     @Autowired private JwtService jwtService;
@@ -54,15 +58,22 @@ public class AuthController {
     }
 
     @PostMapping("/login")
-    public ResponseEntity<String> login(@RequestBody LoginRequest request) {
-        // Authenticates the user against database credentials
-        authenticationManager.authenticate(
-            new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword())
-        );
-        
-        // If authentication passes, generate a token
-        String token = jwtService.generateToken(request.getEmail());
-        return ResponseEntity.ok(token);
+    public ResponseEntity<AuthResponse> login(@RequestBody LoginRequest request) {
+    	try {
+	        // Authenticates the user against database credentials
+	        authenticationManager.authenticate(
+	            new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword())
+	        );
+	        
+	        // If authentication passes, generate a token
+	        String token = jwtService.generateToken(request.getEmail());
+	        
+	        
+	        return ResponseEntity.ok(new AuthResponse(token, "bearer"));
+    	}
+    	catch(BadCredentialsException ex) {
+    		return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+    	}
     }
 }
 
---
+++ src/main/java/com/reaplm/classifiedsapi/dto/AuthResponse.java
@@ -0,0 +1,3 @@
+package com.reaplm.classifiedsapi.dto;
+
+public record AuthResponse(String accessToken, String tokenType) {}
